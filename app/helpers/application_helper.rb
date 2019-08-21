9c4ee4586e3b6928758060420eb2ce1c49a4b3c4
Lareb <lareb.nawab@forgeahead.io>
2019-08-21 13:20:45 +0530

    re-arrange js load path.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,13 +27,14 @@ module ApplicationHelper
 
     return %{
       <!--PREVIEW-FORMS-->
-      <div class="navbar"><a href="#" onclick="return saveAsNewVersion()">Save As New Version</a><a href="#" onclick="return updateVersion()">Update Version</a><a href="#" onclick="return freezVersion()">Freez this version</a><a href="#{template_translation_text_versions_path(template_text.template, template_text)}">Back</a><a href="#" onclick="return generatePDF('#{pdf_template_translation_text_version_path(template_text.template, template_text, version)}', '#{download_template_translation_text_version_url(template_text.template, template_text, version)}')">PDF</a></div>
+      <div class="navbar"><a href="#" onclick="return saveAsNewVersion()">Save As New Version</a><a href="#" onclick="return updateVersion()">Update Version</a><a href="#" onclick="return freezVersion()">Freez this version</a><a href="#" onclick="return generatePDF('#{pdf_template_translation_text_version_path(template_text.template, template_text, version)}', '#{download_template_translation_text_version_url(template_text.template, template_text, version)}')">Download PDF</a><a href="#{template_translation_text_versions_path(template_text.template, template_text)}">Back</a></div>
       <input type="hidden" value="#{template_text.id}" id="template_text_id" />
       <input type="hidden" value="#{template_translation_text_version_path(template_text.template, template_text, version)}" id="edit_form_url" />
       <input type="hidden" value="#{template_translation_text_versions_path(template_text.template, template_text)}" id="new_form_url" />
       <input type="hidden" value="#{freez_template_translation_text_version_path(template_text.template, template_text, version)}" id="freez_version_url" />
       <div id='parent' style='display:none;'></div>
       <!--/PREVIEW-FORMS-->
+      <script src="/js/bootstrap-editable.js"></script>
     }
   end
 
@@ -48,9 +49,9 @@ module ApplicationHelper
       <link href="/css/bootstrap.css" rel="stylesheet">
       <link href="/css/bootstrap-responsive.css" rel="stylesheet">
       #{csrf_meta_tag}
-      <script src="/js/jquery-1.9.1.min.js"csrf-token></script>
+      <script src="/js/jquery.min.js"></script>
       <script src="/js/bootstrap.js"></script>
-      <script src="/js/bootstrap-editable.js"></script>
+
       <script src="/js/html2canvas.js"></script>
       <script src="/js/jspdf.min.js"></script>
       <script src="/js/preview.js"></script>
